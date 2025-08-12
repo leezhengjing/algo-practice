c70403d7d73254dee34ee753bda0b65af314d69c
leezhengjing <zhengjing.lee86@gmail.com>
2025-08-12 20:12:01 +0800

    feat: finish visible_tree_node

---
+++ problems/dfs/on_tree/visible_tree_node/attempt.cpp
@@ -0,0 +1,72 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <sstream>
+#include <string>
+#include <vector>
+
+template<typename T>
+struct Node {
+	T val;
+	Node<T>* left;
+	Node<T>* right;
+
+	explicit Node(T val, Node<T>* left = nullptr, Node<T>* right = nullptr)
+		: val{val}, left{left}, right{right} {}
+
+	~Node() {
+		delete left;
+		delete right;
+	}
+};
+
+int dfs(Node<int>* root, int highest) {
+	if (root == nullptr) {
+		return 0;
+	}
+	int res = 0;
+	if (root->val >= highest) {
+		res++;
+	}
+	res += dfs(root->left, std::max(root->val, highest));
+	res += dfs(root->right, std::max(root->val, highest));
+	return res;
+}
+
+int visible_tree_node(Node<int>* root) {
+	// WRITE YOUR BRILLIANT CODE HERE
+	return dfs(root, std::numeric_limits<int>::min());
+	
+}
+
+// this function builds a tree from input
+// learn more about how trees are encoded in https://algo.monster/problems/serializing_tree
+template<typename T, typename Iter, typename F>
+Node<T>* build_tree(Iter& it, F f) {
+	std::string val = *it;
+	++it;
+	if (val == "x") return nullptr;
+	Node<T>* left = build_tree<T>(it, f);
+	Node<T>* right = build_tree<T>(it, f);
+	return new Node<T>{f(val), left, right};
+}
+
+template<typename T>
+std::vector<T> get_words() {
+	std::string line;
+	std::getline(std::cin, line);
+	std::istringstream ss{line};
+	ss >> std::boolalpha;
+	std::vector<T> v;
+	std::copy(std::istream_iterator<T>{ss}, std::istream_iterator<T>{}, std::back_inserter(v));
+	return v;
+}
+
+int main() {
+	std::vector<std::string> root_vec = get_words<std::string>();
+	auto root_it = root_vec.begin();
+	Node<int>* root = build_tree<int>(root_it, [](auto s) { return std::stoi(s); });
+	int res = visible_tree_node(root);
+	std::cout << res << '\n';
+}
